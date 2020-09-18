d6bba4d5adbe256d3e1ea30a2955878efec5783d
andras-szesztai <andras@newzoo.com>
2020-09-18 18:43:51 +0200

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ["Gilroy", "sans-serif"],
+      body: ["Graphik", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        dark: {
+          primary: "#303242"
+        },
+        light: {
+          primary: "#FFFFFE"
+        },
+        accent: {
+          primary: "#A25A53"
+        }
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
